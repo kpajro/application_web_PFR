0d4bac0b04de93b8b50a520d727a999aa970c45b
leosu1 <leopirrotta14@gmail.com>
2025-02-11 12:12:52 +0100

    améliorations formulaire d'inscription (contraintes et labels sur tous les champs)

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -23,23 +23,75 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, [])
-            ->add('firstname', TextType::class, [])
-            ->add('name', TextType::class, [])
-            ->add('birthday', DateType::class, [])
-            ->add('phoneNumber', TextType::class, [])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse e-mail',
+                'constraints' => [
+                    new Length([
+                        'max' => 100,
+                        'min' => 5,
+                        'message' => 'L\'adresse e-mail doit contenir entre 5 et 100 caractères.'
+                    ]),
+                    new Regex([
+                        'pattern' => '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}.*$',
+                        'match' => true,
+                        'message' => 'L\'adresse e-mail doit finir par un nom de domaine valide (".fr", ".com", ".net", etc.).'
+                    ])
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'constraints' => [
+                    new Length([
+                        'max' => 50,
+                        'min' => 2,
+                        'message' => 'Le prénom doit contenir entre 2 et 50 caractères.'
+                    ])
+                ]
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'constraints' => [
+                    new Length([
+                        'max' => 50,
+                        'min' => 2,
+                        'message' => 'Le nom de famille doit contenir entre 2 et 50 caractères.'
+                    ])
+                ]
+            ])
+            ->add('birthday', DateType::class, [
+                'label' => 'Date de naissance',
+                'constraints' => [
+                ]
+            ])
+            ->add('phoneNumber', TextType::class, [
+                'label' => 'Numéro de téléphone',
+                'constraints' => [
+                    new Length([
+                        'max' => 15,
+                        'min' => 10,
+                        'message' => 'Votre numéro de téléphone doit contenir 10 et 15 caractères.'
+                    ]),
+                    new Regex([
+                        'pattern' => '^\+?[0-9\s]+$',
+                        'match' => true,
+                        'message' => 'Le numéro de téléphone ne peut contenir que des chiffres, des éspaces et le caractère "+".'
+                    ])
+                ]
+            ])
             ->add('country', ChoiceType::class, [
+                'label' => 'Pays de résidence',
                 'choices' => [
                     'France' => 'FR',
-                    'United Kingdom' => 'UK',
-                    'United States' => 'USA',
-                    'Italia' => 'IT',
+                    'Royaume Uni' => 'UK',
+                    'Etats Unis' => 'USA',
+                    'Italie' => 'IT',
                 ],
                 'multiple' => false,
                 'expanded' => false,
                 'placeholder' => 'Séléctionnez votre pays de résidence.'
             ])
             ->add('accountType', ChoiceType::class, [
+                'label' => 'Type de compte',
                 'choices' => [
                     'Compte pour entreprise' => 1,
                     'Compte pour particulier' => 2
@@ -48,8 +100,12 @@ class RegistrationFormType extends AbstractType
                 'expanded' => true
             ])
             ->add('plainPassword', RepeatedType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+                'first_options' => [
+                    'label' => 'Mot de passe'
+                ],
+                'second_options' => [
+                    'label' => 'Je confirme le mot de passe'
+                ],
                 'type' => PasswordType::class,
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
@@ -65,6 +121,7 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => "J'accepte les conditions d'utilisation du site",
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
