6cc6f375c3e3c646936c18e475fadfa88dab82a9
PIRROTTA Léo <leopirrotta14@gmail.com>
2025-02-18 11:01:24 +0100

    amélioration de la confirmation de suppression

--- assets/controllers/confdelete_controller.js
+++ assets/controllers/confdelete_controller.js
@@ -2,20 +2,27 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
     connect() {
+        const footerContent = document.getElementById('footer-content');
         const deleteBtn = document.getElementById('delete-btn');
         const confBox = document.getElementById('delete-conf');
         const cancelBtn = document.getElementById('cancel-delete');
 
         deleteBtn.addEventListener('click', e => {
             e.preventDefault();
-            confBox.classList.remove('hidden');
-            confBox.classList.remove('scale-y-0');
+            footerContent.classList.add('opacity-0');
+            setTimeout(() => {
+                footerContent.classList.add('hidden');
+                confBox.classList.remove('hidden');
+                confBox.classList.remove('opacity-0');
+            }, 150);
 
             cancelBtn.addEventListener('click', e => {
                 e.preventDefault();
-                confBox.classList.add('scale-y-0');
+                confBox.classList.add('opacity-0');
                 setTimeout(() => {
                     confBox.classList.add('hidden');
+                    footerContent.classList.remove('hidden');
+                    footerContent.classList.remove('opacity-0');
                 }, 150);
             })
         })
