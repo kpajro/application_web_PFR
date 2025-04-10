4f53344ef8f590c27bfba1db1203f2ba64de962a
ihavesandineyes <fudempror@gmail.com>
2025-04-10 21:13:10 +0200

    maj about et faq

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
-
+use App\Repository\ParametreSiteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AboutController extends AbstractController{
-    #[Route('/about', name:'app_about', methods:'GET')]
-    public function about(): Response
+    #[Route('/about', name:'app_about')]
+    public function about(EntityManagerInterface $em, ParametreSiteRepository $parametreSiteRepository, Request $request): Response
     {
-        return $this->render('about/about.html.twig');
+        $about = $parametreSiteRepository->find(1);
+
+        if ($request->isMethod('POST')){
+            $about->setDescription($request->request->get('description'));
+            $em->persist($about);
+            $em->flush();
+        }
+
+        return $this->render('about/about.html.twig', [
+            'about'=> $about
+        ]);
     }
 }
--- src/Controller/FAQController.php
+++ src/Controller/FAQController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Repository\FaqRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,8 +11,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 class FAQController extends AbstractController{
     #[Route('/faq', name:'app_faq', methods:'GET')]
-    public function faq(): Response
+    public function faq(FaqRepository $faqRepository): Response
     {
-        return $this->render('faq/faq.html.twig');
+        $categories = $faqRepository->FindAll();
+        return $this->render('faq/faq.html.twig', [
+            'categories'=> $categories
+        ]);
     }
 }
