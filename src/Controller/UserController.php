52c0020caa1964df663f3b9fa50d8055cdb77724
Kiraonigami <sofiane.boudet1@gmail.com>
2025-04-18 12:15:51 +0200

    Test mail

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,30 +8,70 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\UserProfileFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Users;
+use App\Repository\UsersRepository;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
+use Symfony\Component\Mailer\MailerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+
 
 
 class UserController extends AbstractController
 {
-    #[Route('/profile', name: 'app_user_profile')]
-    public function profile(): Response
-    {
-        return $this->render('user/profile.html.twig');
-    }
-
-    #[Route('/profile/parametres', name: 'app_user_settings')]
-    public function account(Request $request, EntityManagerInterface $em): Response
+    #[Route('/profile/{id}', name: 'app_user_profile')]
+    public function profile(int $id, UsersRepository $usersRepo): Response
     {
         $user = $this->getUser();
-        $form = $this->createForm(UserProfileFormType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em->persist($user);
-            $em->flush(); // enregistre les modifs
-            $this->addFlash('success', 'Profile updated successfully!');
+    
+        if (!$user) {
+            throw new AccessDeniedException('Vous devez être connecté pour voir ce profil.');
         }
-
-        return $this->redirectToRoute('app_user_profile');
+    
+        $targetUser = $usersRepo->find($id);
+    
+        if (!$targetUser) {
+            throw new NotFoundHttpException('Utilisateur non trouvé');
+        }
+    
+        if ($user !== $targetUser) {
+            throw new AccessDeniedException('Vous ne pouvez accéder qu’à votre propre profil.');
+        }
+    
+        return $this->render('user/profile.html.twig', [
+            'user' => $targetUser,
+            'form' => $this->createForm(UserProfileFormType::class, $targetUser)->createView()
+        ]);
+    }
+    
+    // Route pour le traitement du formulaire (AJAX ou POST depuis la modale)
+    #[Route('/profile/{id}/parametre', name: 'app_user_profile_update')]
+    public function updateProfile(int $id, Request $request, UsersRepository $usersRepo, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+    
+        if (!$user) {
+            throw new AccessDeniedException();
+        }
+    
+        $targetUser = $usersRepo->find($id);
+    
+        if (!$targetUser || $user !== $targetUser) {
+            throw new AccessDeniedException();
+        }
+    
+        $form = $this->createForm(UserProfileFormType::class, $targetUser);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Profil mis à jour.');
+        }
+    
+        return $this->redirectToRoute('app_user_profile', ['id' => $id]);
     }
 
 
@@ -46,4 +86,30 @@ class UserController extends AbstractController
     // {
     //     return $this->render('commandes.html.twig');
     // }
+
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    #[Route('/test-mail', name: 'test_mail')]
+    public function testMail(MailerInterface $mailer): Response
+    {
+        $email = (new TemplatedEmail())
+            ->from('no_reply@projetfilrouge.com')
+            ->to('ton-email@exemple.com')
+            ->subject('Test Email')
+            ->html('<p>Ce message est un test de l\'envoi d\'email.</p>');
+        
+        try {
+            $mailer->send($email);
+            $this->logger->info('Email envoyé à : ton-email@exemple.com'); // Utilisation du logger injecté
+        } catch (\Exception $e) {
+            $this->logger->error('Erreur lors de l\'envoi de l\'email: '.$e->getMessage()); // Utilisation du logger injecté
+        }
+
+        return new Response('Email envoyé!');
+    }
 }
