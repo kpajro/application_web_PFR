d23bc86484335adffc73efc9b4d629a09e7cfc76
ihavesandineyes <fudempror@gmail.com>
2025-04-25 14:16:24 +0200

    peaufinage controlleur a propos

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ParametreSiteRepository;
+use App\Entity\ParametreSite;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +14,21 @@ class AboutController extends AbstractController{
     #[Route('/about', name:'app_about')]
     public function about(EntityManagerInterface $em, ParametreSiteRepository $parametreSiteRepository, Request $request): Response
     {
-        $about = $parametreSiteRepository->find(1);
+        $parametres = $parametreSiteRepository->findAll();
+        $about = $parametres[0] ?? null;
+
+        if (!$about){
+            $about = new ParametreSite();
+            $about->setAdresseSupport('');
+            $about->setDescription('');
+            $about->setAdresseEmail('');
+            $em->persist($about);
+            $em->flush();
+        }
 
         if ($request->isMethod('POST')){
-            $about->setDescription($request->request->get('description'));
+            $nouveldesc = $request->request->get('description');
+            $about->setDescription($nouveldesc);
             $em->persist($about);
             $em->flush();
         }
