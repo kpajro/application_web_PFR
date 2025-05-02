a694a34abdc0481f9ff0db0bc467b528306e82bf
Léo Pirrotta <leopirrotta14@gmail.com>
2025-05-02 19:14:30 +0200

    fonction ajouter au panier en js

--- assets/controllers/produits_controller.js
+++ assets/controllers/produits_controller.js
@@ -7,6 +7,9 @@ export default class extends Controller {
 
     changeSelectedContent(event) {
         const currentSelected = document.querySelector('.produit-content-selected');
+        if (currentSelected === event.currentTarget) {
+            return;
+        }
         currentSelected.classList.remove('produit-content-selected');
         setTimeout(() => {
             event.currentTarget.classList.add('produit-content-selected');
@@ -15,6 +18,24 @@ export default class extends Controller {
 
     addToCart(event) {
         event.preventDefault();
-        console.log(event.currentTarget);
+        const url = event.currentTarget.dataset.url;
+        const confBox = document.getElementById('confirmation');
+
+        fetch(url).then(response => {
+            if (!response.ok) {
+                confBox.classList.remove('bg-green-300/60');
+                confBox.classList.remove('text-green-700');
+                confBox.classList.add('text-rose-700');
+                confBox.classList.add("bg-rose-300/60");
+                confBox.classList.remove("hidden");
+                confBox.innerText = "Erreur lors de l'ajout du produit au panier"; 
+                throw new Error("Erreur lors de l'ajout du produit au panier")
+            }
+
+            return response.text();
+        }).then(text => {
+            confBox.classList.remove('hidden');
+            confBox.innerHTML = "<i class='fa-solid fa-circle-check mr-2'></i>" + text;
+        })
     }
 }
