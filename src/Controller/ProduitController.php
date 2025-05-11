e410168a0e422d9447f15465819d29dc1dc0da5f
Léo Pirrotta <leopirrotta14@gmail.com>
2025-05-11 21:57:56 +0200

    début de la route pour laisser un avis

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Avis;
 use App\Entity\Produit;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,6 +18,24 @@ class ProduitController extends AbstractController
     {
         return $this->render('produit/view.html.twig', [
             'produit' => $produit
+        ]);
+    }
+
+    #[Route('/produit/{id}/donner-avis', name: 'app_produit_donner_avis')]
+    public function donnerAvis(Produit $produit, Request $request, EntityManagerInterface $em): Response
+    {
+        $avis = new Avis();
+        $form = $this->createForm(AvisFormType::class, $avis, [
+            'action' => $this->generateUrl('app_produit_donner_avis', ['id' => $produit->getId()])
+        ]);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+        }
+
+        return $this->render('/produit/donner-avis.html.twig', [
+            'produit' => $produit,
+            'form' => $form
         ]);
     }
 }
