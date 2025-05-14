d7b6819f5871bac3e18ab5682f278e29ef27f344
PIRROTTA Léo <leopirrotta14@gmail.com>
2025-05-14 11:24:47 +0200

    possibilité de changer la taille de la modale (attribut data-modal-size)

--- assets/controllers/modal_controller.js
+++ assets/controllers/modal_controller.js
@@ -8,10 +8,24 @@ export default class extends Controller {
     open(event) {
         event.preventDefault();
         const url = event.currentTarget.dataset.modalUrl;
+        const size = event.currentTarget.dataset.modalSize;
         const modal = document.getElementById("modal");
         const box = document.getElementById("modal-box");
-        
         modal.classList.remove("hidden"); // on affiche la modale
+        switch (size) {
+            case 'sm':
+                box.classList.add('max-w-3xl');
+                break;
+            case 'md':
+                box.classList.add('max-w-5xl');
+                break;
+            case 'lg':
+                box.classList.add('max-w-7xl');
+                break;
+            case 'xl':
+                box.classList.add('max-w-[1580px]');
+                break;
+        }
 
         fetch(url, { method: "POST" }) //requête pour récupérer le contenu voulu
             .then((response) => {
@@ -23,7 +37,7 @@ export default class extends Controller {
             .then(html => {
                     this.element.querySelector("#modal-content").innerHTML = html; //le contenu chargé est injecté dans la modale
                     this.closeModalHandler(modal, box); //on charge les boutons pour fermer la modale
-
+                    
                     modal.classList.remove("opacity-0");
                     box.classList.remove("translate-y-[100vh]");
                 })
@@ -43,28 +57,34 @@ export default class extends Controller {
         // Masquer la modale
         box.classList.add("translate-y-[100vh]");
         modal.classList.add('opacity-0');
+        console.log(event)
 
         setTimeout(() => {
             modal.classList.add("hidden");
             // Nettoyer le contenu
             modal.querySelector("#modal-content").innerHTML = "";
+            this.removeSize(box);
         }, 200);
     }
 
     closeModalHandler(modal, box) {
         const closeButtons = this.element.querySelectorAll(".close-modal");
-
         closeButtons.forEach((button) => {
             button.addEventListener("click", (event) => {
                 this.close(event, modal, box);
             });
         });
-
         document.addEventListener('click', e => {
             if (typeof e.composedPath === 'function' &&  !e.composedPath().includes(box) && !box.classList.contains('translate-y-[100vh]')) {
                 this.close(e, modal, box);
             }
         })
+    }
+
+    removeSize(box) {
+        const classes = [...box.classList];
+        const sizeClass = classes.find(cls => cls.startsWith('max-w-'));
+        box.classList.remove(sizeClass);
     }
 
     updateNavButtons () {
