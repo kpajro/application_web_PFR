8ca217078740241a8464c100a3aa3cf9855ebf22
pajro <147093238+kpajro@users.noreply.github.com>
2025-05-27 11:46:33 +0200

    création de session de paiement stripe

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -21,47 +21,47 @@ class CheckoutController extends AbstractController
     #[Route('/create-checkout', name: 'app_checkout', methods: ['POST'])]
     public function createCheckoutSession(Request $request): JsonResponse
     {
-        
+        // temporaire pour les tests (ou sinon osef)
         \Stripe\Stripe::setApiKey('rk_test_51RSvQkRVumHN60oo3B0poNqglD7FJgwbKRuRrVSsbf1TAuXMQHY7QJTXxjnmblJf7Do95CZSBlUt92qMiIusTwPX00uMhWtTe7');
+
+        $panier = $this->panierHandler->getActivePanier($this->getUser(), $request);
+        //~#Klaudiusz vvv pourquoi je dois faire ca pour initialiser le panier ???
+        $this->panierHandler->getPanierTotalPrice($panier);
+        $panierProduits = $panier->getPanierProduits();
+
+        $lineItems = [];
+
+        foreach($panierProduits as $panierProduit){
+            $produit = $panierProduit->getProduit();
+            $lineItems[] = [
+                'price_data' => [
+                    'currency' => 'eur',
+                    'unit_amount' => $produit->getPrix() * 100,
+                    'product_data' => [
+                        'name' => $produit->getNom(),
+                    ],
+                ],
+                'quantity' => $panierProduit->getAmount(),
+            ];
+        }
 
         $session = \Stripe\Checkout\Session::create([
             'payment_method_types' => ['card'],
-            'line_items' => [[
-                'price_data' => [
-                    'currency' => 'eur',
-                    'unit_amount' => 1000, // 10,00 €
-                    'product_data' => [
-                        'name' => 'Produit Exemple',
-                    ],
-                ],
-                'quantity' => 1,
-            ],
-            [
-                'price_data' => [
-                    'currency' => 'eur',
-                    'unit_amount' => 3500,
-                    'product_data' => [
-                        'name' => 'Mircosoft 720'
-                    ],
-                ],
-                'quantity' => 30,
-            ]
-        ],
-
+            'line_items' => $lineItems,
             'mode' => 'payment',
-            'success_url' => $this->generateUrl('payment_success', [], UrlGeneratorInterface::ABSOLUTE_URL),
-            'cancel_url' => $this->generateUrl('payment_cancel', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'success_url' => $this->generateUrl('app_payment_success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('app_payment_cancel', [], UrlGeneratorInterface::ABSOLUTE_URL),
         ]);
 
         return new JsonResponse(['id' => $session->id]);
     }
+
     #[Route('/checkout', name: 'app_checkout_page')]
     public function checkoutPage(Request $request): Response
     {
-        $panier = $this->panierHandler->getActivePanier($this->getUser(), $request);
-        dd($panier);
-        return $this->render('checkout/checkout.html.twig', [
-            'stripe_public_key' => 'pk_test_51RSvQkRVumHN60ooKlCL6qUPaVblzy3dtuAP3XwdF8LChY4G56VLJKpi526WBpi3VUEy0XcJifynKetmnul5Us7100AS1ThEJH',
+        // temporaire pour les tests (ou sinon osef)
+        return $this->render('/checkout/checkout.html.twig', [
+            'stripe_public_key' => 'pk_test_51RSvQkRVumHN60ooKlCL6qUPaVblzy3dtuAP3XwdF8LChY4G56VLJKpi526WBpi3VUEy0XcJifynKetmnul5Us7100AS1ThEJH',     
         ]);
     }
     #[Route('/payment-success', name: 'app_payment_success')]
