697612fc74d2516d2ea6cc58302e0da554a1af8e
pajro <147093238+kpajro@users.noreply.github.com>
2025-05-28 17:17:24 +0200

    route succès paiement stripe

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Panier;
+use App\Entity\Paiement;
+use App\Entity\Users;
 use App\Service\PanierHandler;
 use App\Repository\PanierRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -12,23 +15,15 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
+
 class CheckoutController extends AbstractController
 {
     public function __construct(private PanierHandler $panierHandler)
     {
     }
 
-    #[Route('/retrieve-payment', name: 'app_stripe_payment_retrieve', methods: ['POST'])]
-    public function retrievePayment(Request $request): JsonResponse
-    {
-        \Stripe\Stripe::setApiKey('rk_test_51RSvQkRVumHN60oofb9knkAD9MuBQ3D4n4BT6IbUeQa8gB2qeOR1figukukv9QfbrAUMutzFrTLPCjx6A5HvSxqU00h6rMO9H4');
-        // A finir pour recupération du paiement et l'insérer dans la base de données
-        $paymentIntent = \Stripe\PaymentIntent::retrieve('');
-        Return new JsonResponse($paymentIntent);
-    }
-
     #[Route('/create-checkout', name: 'app_stripe_checkout_create', methods: ['POST'])]
-    public function createCheckoutSession(Request $request): JsonResponse
+    public function createCheckoutSession(Request $request, EntityManagerInterface $em): JsonResponse
     {
         // temporaire pour les tests (ou sinon osef)
         \Stripe\Stripe::setApiKey('rk_test_51RSvQkRVumHN60oo3B0poNqglD7FJgwbKRuRrVSsbf1TAuXMQHY7QJTXxjnmblJf7Do95CZSBlUt92qMiIusTwPX00uMhWtTe7');
@@ -58,7 +53,7 @@ class CheckoutController extends AbstractController
             'payment_method_types' => ['card'],
             'line_items' => $lineItems,
             'mode' => 'payment',
-            'success_url' => $this->generateUrl('app_stripe_payment_success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'success_url' => $this->generateUrl('app_stripe_payment_success', [], UrlGeneratorInterface::ABSOLUTE_URL) . '?session_id={CHECKOUT_SESSION_ID}',
             'cancel_url' => $this->generateUrl('app_stripe_payment_cancel', [], UrlGeneratorInterface::ABSOLUTE_URL),
         ]);
 
@@ -70,12 +65,36 @@ class CheckoutController extends AbstractController
     {
         // temporaire pour les tests (ou sinon osef)
         return $this->render('/checkout/checkout.html.twig', [
-            'stripe_public_key' => 'pk_test_51RSvQkRVumHN60ooKlCL6qUPaVblzy3dtuAP3XwdF8LChY4G56VLJKpi526WBpi3VUEy0XcJifynKetmnul5Us7100AS1ThEJH',     
+            'stripe_public_key' => 'pk_test_51RSvQkRVumHN60ooKlCL6qUPaVblzy3dtuAP3XwdF8LChY4G56VLJKpi526WBpi3VUEy0XcJifynKetmnul5Us7100AS1ThEJH',
         ]);
     }
     #[Route('/payment-success', name: 'app_stripe_payment_success')]
-    public function paymentSuccess(): Response
+    public function paymentSuccess(Request $request, EntityManagerInterface $em): Response
     {
+        // faire verif si l'utilisateur connecté et vient de faire un paiement
+        $loggedUser = $this->getUser();
+        if ($loggedUser){
+            \Stripe\Stripe::setApiKey("rk_test_51RSvQkRVumHN60oo3B0poNqglD7FJgwbKRuRrVSsbf1TAuXMQHY7QJTXxjnmblJf7Do95CZSBlUt92qMiIusTwPX00uMhWtTe7");
+            $sessionId = $request->query->get("session_id");
+            $session = \Stripe\Checkout\Session::retrieve($sessionId);
+            $paymentIntent = \Stripe\PaymentIntent::retrieve($session->payment_intent);
+            
+            $paiement = new Paiement($loggedUser);
+            $uuid = $paymentIntent->id;
+            $montant = $paymentIntent->amount / 100;
+            $date = (new \DateTime())->setTimestamp($paymentIntent->created);
+            $status = $paymentIntent->status;
+
+            $paiement->setUuid($uuid);
+            $paiement->setMontant($montant);
+            $paiement->setDate($date);
+            $paiement->setStatus($status);
+            $paiement->setUserId($loggedUser);
+    
+            $em->persist($paiement);
+            $em->flush();
+        }
+
         return $this->render('checkout/success.html.twig');
     }
 
