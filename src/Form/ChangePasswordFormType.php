6c927928788f36a54f04e1400e99fc8c82d59329
Léo Pirrotta <leopirrotta14@gmail.com>
2025-08-22 14:52:49 +0200

    fix de la modification de mot de passe et peaufinage de la présentation

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -8,21 +8,34 @@ use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class ChangePasswordFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('plainPassword', RepeatedType::class, [
-            'type' => PasswordType::class,
-            'mapped' => false,
-            'first_options'  => ['label' => 'Nouveau mot de passe'],
-            'second_options' => ['label' => 'Confirmez le mot de passe'],
-            'invalid_message' => 'Les mots de passe doivent correspondre.',
-            'constraints' => [
-                new NotBlank(['message' => 'Veuillez entrer un mot de passe']),
-                new Length(['min' => 8, 'minMessage' => 'Minimum {{ limit }} caractères']),
-            ],
-        ]);
+        $builder
+            ->add('plainOldPassword', PasswordType::class, [
+                'label' => "Mot de passe actuel",
+                'label_attr' => ['font-medium mb-2'],
+            ])
+            ->add('plainNewPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'first_options'  => ['label' => 'Nouveau mot de passe'],
+                'second_options' => ['label' => 'Confirmez le mot de passe'],
+                'invalid_message' => 'Les mots de passe doivent correspondre.',
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer un mot de passe.']),
+                    new Regex([
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d;:*\\\\\/{}]).{8,}$/',     // regex pour mdp robuste : 6 caractères minimum, une maj, une  min, un chiffre et un caractère spécial (en excluant ceux qui peuvent être dangereux)
+                        'match' => true,
+                        'message' => 'Le mot de passe doit contenir au minimum 6 caractères, une minuscule, une majuscule, un chiffre et un caractère spécial.'
+                    ])
+                ],
+                'label_attr' => ['font-medium mb-2'],
+                'attr' => ['class' => 'mb-3'],
+            ])
+        ;
     }
 }
