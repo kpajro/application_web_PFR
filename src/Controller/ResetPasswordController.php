b052822f0552ab8bacf3175d73c10c3ba1d7aca9
Léo Pirrotta <leopirrotta14@gmail.com>
2025-08-22 12:59:04 +0200

    traduction objet de l'email pour reset pwd

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -130,7 +130,7 @@ class ResetPasswordController extends AbstractController
         $email = (new TemplatedEmail())
             ->from(new Address('sofiane.boudet1@gmail.com', 'Réinitialisation de votre mot de passe'))
             ->to((string) $user->getEmail())
-            ->subject('Your password reset request')
+            ->subject('Votre demande de réinitialisation de mot de passe')
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
                 'resetToken' => $resetToken,
